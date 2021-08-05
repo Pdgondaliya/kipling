0fe63907cc7dd1c973333d8a6ca865f267f4fa11
Jaimin  Bhikadiya <jaiminbhikadiya9@gmail.com>
2021-08-05 12:54:29 +0530

    font size changed for text field

--- lib/custom_widget/text_field.dart
+++ lib/custom_widget/text_field.dart
@@ -3,33 +3,33 @@ import 'package:kipling/MediaQuery/get_mediaquery.dart';
 
 Widget buildtextfields(
     {required String hint,
-      required TextEditingController controller,
-      required BuildContext context,
-      bool? suffix,
-      bool enable = true,
-      FormFieldValidator<String>? validator,
-      Icon? suffixIcon,
-      double? width,
-      Function()? onTap,
-      bool? isPassword,
-      TextInputType? keyboard}) {
+    required TextEditingController controller,
+    required BuildContext context,
+    bool? suffix,
+    bool enable = true,
+    FormFieldValidator<String>? validator,
+    Icon? suffixIcon,
+    double? width,
+    Function()? onTap,
+    bool? isPassword,
+    TextInputType? keyboard}) {
   return GestureDetector(
     onTap: onTap,
     child: Container(
       height: displayHeight(context) * 0.07,
       width: width == null ? double.infinity : width,
       child: TextFormField(
-          style: TextStyle(color: Color(0xff010001)),
+          style: TextStyle(
+              color: Color(0xff010001), fontSize: displayWidth(context) * 0.06),
           keyboardType: keyboard,
           controller: controller,
           obscureText: isPassword == true ? true : false,
           validator: validator,
-
           decoration: InputDecoration(
               contentPadding: EdgeInsets.symmetric(horizontal: 10),
               hintText: hint,
               hintStyle:
-              const TextStyle(fontSize: 17, color: Color(0xff9f9e9f)),
+                  const TextStyle(fontSize: 17, color: Color(0xff9f9e9f)),
               border: InputBorder.none,
               focusedBorder: const OutlineInputBorder(
                 borderSide: BorderSide(color: Colors.black38, width: 1.0),
