323aa1edc29b2dc05754130220d75f550c950a92
Jaimin  Bhikadiya <jaiminbhikadiya9@gmail.com>
2021-08-11 14:47:57 +0530

    bug fixed

--- lib/ui/forgot_password.dart
+++ lib/ui/forgot_password.dart
@@ -117,131 +117,134 @@ class _ForgotPasswordState extends State<ForgotPassword> {
               right: displayWidth(context) * 0.08,
               left: displayWidth(context) * 0.08,
               top: displayWidth(context) * 0.4),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                index == 0
-                    ? ld!.value!.titleTextEn.toString()
-                    : ld!.value!.titleTextNl.toString(),
-                style: TextStyle(
-                    fontFamily: 'Kipling_Bold',
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  index == 0
+                      ? ld!.value!.titleTextEn.toString()
+                      : ld!.value!.titleTextNl.toString(),
+                  style: TextStyle(
+                      fontFamily: 'Kipling_Bold',
+                      color: Colors.black,
+                      fontSize: displayWidth(context) * 0.08),
+                  textAlign: TextAlign.start,
+                ),
+                SizedBox(height: displayWidth(context) * 0.05),
+                Text(
+                  index == 0
+                      ? ld!.value!.descriptionTextEn.toString()
+                      : ld!.value!.descriptionTextNl.toString(),
+                  style: TextStyle(
                     color: Colors.black,
-                    fontSize: displayWidth(context) * 0.08),
-                textAlign: TextAlign.start,
-              ),
-              SizedBox(height: displayWidth(context) * 0.05),
-              Text(
-                index == 0
-                    ? ld!.value!.descriptionTextEn.toString()
-                    : ld!.value!.descriptionTextNl.toString(),
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: displayWidth(context) * 0.05,
-                  fontFamily: 'Kipling_Regular',
+                    fontSize: displayWidth(context) * 0.05,
+                    fontFamily: 'Kipling_Regular',
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: displayWidth(context) * 0.04,
-                  // horizontal: displayWidth(context) * 0.04,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      index == 0
-                          ? ld!.value!.fields!.emailAddress!.titleTextEn
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: displayWidth(context) * 0.04,
+                    // horizontal: displayWidth(context) * 0.04,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        index == 0
+                            ? ld!.value!.fields!.emailAddress!.titleTextEn
+                            .toString()
+                            : ld!.value!.fields!.emailAddress!.titleTextNl
+                            .toString(),
+                        style: TextStyle(
+                            color: Color(0xff010001),
+                            fontSize: displayWidth(context) * 0.05,
+                            fontFamily: 'Kipling_Regular'),
+                      ),
+                      SizedBox(height: 5),
+                      buildtextfields(
+                          context: context,
+                          controller: emailController,
+                          hint: index == 0
+                              ? ld!.value!.fields!.emailAddress!.placeholderTextEn
                               .toString()
-                          : ld!.value!.fields!.emailAddress!.titleTextNl
+                              : ld!.value!.fields!.emailAddress!.placeholderTextNl
                               .toString(),
-                      style: TextStyle(
-                          color: Color(0xff010001),
-                          fontSize: displayWidth(context) * 0.05,
-                          fontFamily: 'Kipling_Regular'),
-                    ),
-                    SizedBox(height: 5),
-                    buildtextfields(
-                        context: context,
-                        controller: emailController,
-                        hint: index == 0
-                            ? ld!.value!.fields!.emailAddress!.placeholderTextEn
-                                .toString()
-                            : ld!.value!.fields!.emailAddress!.placeholderTextNl
-                                .toString(),
-                        onChanged: (value) {
-                          if (value != '') {
-                            setState(() {
-                              emailError = '';
-                            });
-                          }
-                        },
-                        keyboard: TextInputType.emailAddress),
-                    emailError == ''
-                        ? Container()
-                        : Container(
-                            child: Text(emailError,
-                                style: TextStyle(
-                                    color: Colors.red,
-                                    fontFamily: 'Kipling_Regular')),
-                          ),
-                    Container(
-                      width: double.infinity,
-                      height: displayHeight(context) * 0.07,
-                      margin:
-                          EdgeInsets.only(top: displayHeight(context) * 0.03),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          if (emailController.text == '') {
-                            setState(() {
-                              emailError = 'Please enter email address';
-                            });
-                          } else if (emailController.text.contains('@') &&
-                              emailController.text.contains('.')) {
-                            emailSendAPI(email: emailController.text)
-                                .then((value) {
-                              print('Value: $value');
-                              if (value == 200) {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            ForgotPasswordConfirmation(
-                                              email: emailController.text,
-                                            )));
-                              } else if (value == 400) {
+                          onChanged: (value) {
+                            if (value != '') {
+                              setState(() {
+                                emailError = '';
+                              });
+                            }
+                          },
+                          keyboard: TextInputType.emailAddress),
+                      emailError == ''
+                          ? Container()
+                          : Container(
+                        child: Text(emailError,
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontFamily: 'Kipling_Regular')),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: displayHeight(context) * 0.07,
+                        margin:
+                        EdgeInsets.only(top: displayHeight(context) * 0.03),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            if (emailController.text == '') {
+                              setState(() {
+                                emailError = 'Please enter email address';
+                              });
+                            } else if (emailController.text.contains('@') &&
+                                emailController.text.contains('.')) {
+                              emailSendAPI(email: emailController.text)
+                                  .then((value) {
+                                print('Value: $value');
+                                if (value == 200) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ForgotPasswordConfirmation(
+                                                email: emailController.text,
+                                              )));
+                                } else if (value == 400) {
 
-                              }
-                            });
-                          } else {
-                            setState(() {
-                              emailError = 'Please enter a valid email address';
-                            });
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: const Color(0xFF2d2c2e),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(0.0)),
-                        ),
-                        child: Text(
-                          index == 0
-                              ? ld!.value!.fields!.cta!.titleTextEn.toString()
-                              : ld!.value!.fields!.cta!.titleTextNl.toString(),
-                          style: TextStyle(
-                              fontSize: displayWidth(context) * 0.05,
-                              color: Color(0xfffcfdfd),
-                              fontFamily: 'Kipling_Regular'),
+                                }
+                              });
+                            } else {
+                              setState(() {
+                                emailError = 'Please enter a valid email address';
+                              });
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF2d2c2e),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(0.0)),
+                          ),
+                          child: Text(
+                            index == 0
+                                ? ld!.value!.fields!.cta!.titleTextEn.toString()
+                                : ld!.value!.fields!.cta!.titleTextNl.toString(),
+                            style: TextStyle(
+                                fontSize: displayWidth(context) * 0.05,
+                                color: Color(0xfffcfdfd),
+                                fontFamily: 'Kipling_Regular'),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          )),
+              ],
+            ),
+          )
+          ),
     );
   }
 }
